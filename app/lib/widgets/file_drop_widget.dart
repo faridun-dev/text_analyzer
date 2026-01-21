c6bc674662d4d72284977d0b382bce0109a09589
Faridun Fatkhullozoda <faridunf200630@gmail.com>
2026-01-21 22:16:20 +0500

    feat: impl file picker on file drop widget

--- app/lib/widgets/file_drop_widget.dart
+++ app/lib/widgets/file_drop_widget.dart
@@ -2,21 +2,47 @@ import 'package:desktop_drop/desktop_drop.dart';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:file_picker/file_picker.dart';
 
 class FileDropWidget extends StatefulWidget {
   bool dragging;
-  FileDropWidget({super.key, required this.dragging});
+  final Function(List<String>)? onFilesSelected; // Callback for selected files
+  
+  FileDropWidget({
+    super.key, 
+    required this.dragging,
+    this.onFilesSelected,
+  });
 
   @override
   State<FileDropWidget> createState() => _FileDropWidgetState();
 }
 
 class _FileDropWidgetState extends State<FileDropWidget> {
+  Future<void> _pickFiles() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      allowMultiple: true,
+      type: FileType.any,
+    );
+
+    if (result != null) {
+      List<String> filePaths = result.paths.whereType<String>().toList();
+      widget.onFilesSelected?.call(filePaths);
+      
+      // Optional: Print selected files for debugging
+      print('Selected files: $filePaths');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return DropTarget(
       onDragDone: (detail) {
-        setState(() {});
+        List<String> filePaths = detail.files.map((file) => file.path).toList();
+        widget.onFilesSelected?.call(filePaths);
+        setState(() {
+          widget.dragging = false;
+        });
       },
       onDragEntered: (detail) {
         setState(() {
@@ -61,7 +87,7 @@ class _FileDropWidgetState extends State<FileDropWidget> {
               ),
               SizedBox(height: 5),
               ElevatedButton(
-                onPressed: () {},
+                onPressed: _pickFiles,
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Color(0xFF1D4ED8),
                   foregroundColor: Colors.white,
